a1d9cd1747cf25e256117cf4fd91367d447a8af9
Remix Workspace <>
2022-09-16 14:22:22 +0300

    Structs

--- contracts/examples/StructExample.sol
+++ contracts/examples/StructExample.sol
@@ -2,6 +2,57 @@
 
 pragma solidity 0.8.17;
 
-contract StructExample {
-    
+contract Wallet {
+
+    PaymentReceived public payment;
+
+    function PayContract() public payable {
+        payment = new PaymentReceived(msg.sender, msg.value);
+    }
+}
+
+contract PaymentReceived {
+    address public from;
+    uint public amount;
+
+    constructor(address _from, uint _amount) {
+        from = _from;
+        amount = _amount;
+    }
+}
+
+contract PaymentReceived2 {
+    struct PaymentReceivedStruct {
+        address from;
+        uint amount;
+    }
+}
+
+// Preferred options below because of the gas cost savings.
+contract Wallet2 {
+    // Good to save costs on gas when using structs.
+    struct PaymentReceivedStruct {
+        address from;
+        uint amount;
+    }
+
+    PaymentReceivedStruct public payment;
+
+    function payContract() public payable {
+        //payment = PaymentReceivedStruct(msg.sender, msg.value);
+        // OR
+        payment.from = msg.sender;
+        payment.amount = msg.value;
+    }
+}
+
+contract Wallet3 is PaymentReceived2 {
+    PaymentReceivedStruct public payment;
+
+    function payContract() public payable {
+        //payment = PaymentReceivedStruct(msg.sender, msg.value);
+        // OR
+        payment.from = msg.sender;
+        payment.amount = msg.value;
+    }
 }
